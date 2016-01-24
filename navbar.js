5bda89353f544f59748f9325ed2a33715d184ad4
megs26 <megs26@email.arizona.edu>
2016-01-24 05:05:10 -0700

    Committing changes to master

--- navbar.js
+++ navbar.js
@@ -4,7 +4,7 @@ document.write('\
 <ul>\
       <div class="block">\
           <li><left><a href="home.html"><img src="KMine Logo.png" height="100"></a></left>\
-          <li><a href="form.html">Submit</a></li>\
+          <right><li><a href="form.html">Submit</a></li>\
           <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="">Adopt!</a>\
                <ul class="dropdown-menu">\
                 <li><a href="browse.html">Browse</a></li>\
@@ -14,7 +14,7 @@ document.write('\
           <li><a href="centers.html">Shelters</a></li>\
           <li><a href="puppycare.html">Puppy Care</a></li>\
           <li><a href="encyclopedia.html">Encyclopedia</a></li>\
-          <li><a href="signin.html">Sign In</a></li>\
+          <li><a href="signin.html">Sign In</a></li></right>\
         </center>\
       </div>\
     </div>\
